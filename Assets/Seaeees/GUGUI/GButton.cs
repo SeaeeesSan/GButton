21a534f04d7446949b9f3657b8f29c4b8ec4533c
Seaeees <seaeees@gmail.com>
2021-03-21 13:39:05 +0900

    Fix:コード整形

--- Assets/Seaeees/GUGUI/GButton.cs
+++ Assets/Seaeees/GUGUI/GButton.cs
@@ -11,19 +11,14 @@ namespace Seaeees.GUGUI
         private Coroutine _scaleAnimationCoroutine;
         private Coroutine _colorAnimationCoroutine;
         private Coroutine _fillAnimationCoroutine;
-        
         private RectTransform _rectTransform;
         private Image _image;
         private Image _fillImage;
-        
         private Color _defaultColor;
         private Vector2 _defaultScale;
         private Sprite _defaultSprite;
-        
         private Vector2 _calculatedScaleOnHover;
         private Vector2 _calculatedScaleOnClick;
-        
-        //[SerializeField] private GButtonTemplate template;
 
         [SerializeField] private bool useScaleAnimationOnHover;
         [SerializeField] private bool useScaleAnimationOnClick;
@@ -33,24 +28,20 @@ namespace Seaeees.GUGUI
         [SerializeField] private bool useFillAmountAnimation;
         [SerializeField] private bool useImageChangerOnHover;
         [SerializeField] private bool useImageChangerOnClick;
-
         [SerializeField] private EaseType scaleEaseType = EaseType.Linear;
         [SerializeField] private Vector2 scaleOnHover;
         [SerializeField] private float scaleDurationOnHover = 0.1f;
         [SerializeField] private Vector2 scaleOnClick;
         [SerializeField] private float scaleDurationOnClick = 0.1f;
-
         [SerializeField] private Color colorOnHover = Color.gray;
         [SerializeField] private float colorDurationOnHover = 0.1f;
         [SerializeField] private Color colorOnClick = Color.gray;
         [SerializeField] private float colorDurationOnClick = 0.1f;
-
         [SerializeField] private AudioSource audioSource;
         [SerializeField] private AudioClip hoverEnterAudioClip;
         [SerializeField] private AudioClip hoverExitAudioClip;
         [SerializeField] private AudioClip downAudioClip;
         [SerializeField] private AudioClip upAudioClip;
-
         [SerializeField] private EaseType fillImageEaseType = EaseType.Linear;
         [SerializeField] private Image fillImage;
         [SerializeField] private float fillImageDuration = 0.1f;
@@ -62,15 +53,13 @@ namespace Seaeees.GUGUI
             _rectTransform = GetComponent<RectTransform>();
             _image = GetComponent<Image>();
             
-            var delta = _rectTransform.sizeDelta;
-
             _fillImage = fillImage;
-            _defaultScale = delta;
+            _defaultScale = _rectTransform.sizeDelta;
             _defaultColor = _image.color;
             _defaultSprite = _image.sprite;
             
-            _calculatedScaleOnHover = new Vector2(delta.x + scaleOnHover.x, delta.y + scaleOnHover.y);
-            _calculatedScaleOnClick = new Vector2(delta.x + scaleOnClick.x, delta.y + scaleOnClick.y);
+            _calculatedScaleOnHover = new Vector2(_defaultScale.x + scaleOnHover.x, _defaultScale.y + scaleOnHover.y);
+            _calculatedScaleOnClick = new Vector2(_defaultScale.x + scaleOnClick.x, _defaultScale.y + scaleOnClick.y);
         }
 
         public void OnPointerEnter(PointerEventData eventData) => PlayButtonEffects(AnimationType.PointerEnter);
@@ -96,92 +85,61 @@ namespace Seaeees.GUGUI
         private void PlayAudio(AnimationType animationType)
         {
             if (!audioSource) return;
-            switch (animationType)
-            {
-                case AnimationType.PointerEnter when useAudioPlayer:
-                    audioSource.PlayOneShot(hoverEnterAudioClip);
-                    break;
-                case AnimationType.PointerExit when useAudioPlayer:
-                    audioSource.PlayOneShot(hoverExitAudioClip);
-                    break;
-                case AnimationType.PointerDown when useAudioPlayer:
-                    audioSource.PlayOneShot(downAudioClip);
-                    break;
-                case AnimationType.PointerUp when useAudioPlayer:
-                    audioSource.PlayOneShot(upAudioClip);
-                    break;
-            }
+            if (!useAudioPlayer) return;
+            if (animationType == AnimationType.PointerEnter)
+                audioSource.PlayOneShot(hoverEnterAudioClip);
+            else if (animationType == AnimationType.PointerExit)
+                audioSource.PlayOneShot(hoverExitAudioClip);
+            else if (animationType == AnimationType.PointerDown)
+                audioSource.PlayOneShot(downAudioClip);
+            else if (animationType == AnimationType.PointerUp) 
+                audioSource.PlayOneShot(upAudioClip);
         }
         
         private void AnimationFillAmount(AnimationType animationType)
         {
             if (!_fillImage) return;
-            switch (animationType)
-            {
-                case AnimationType.PointerEnter when useFillAmountAnimation:
-                    ResetCoroutine(ref _fillAnimationCoroutine, _fillImage.AnimateFillAmount(1, fillImageDuration, fillImageEaseType));
-                    break;
-                case AnimationType.PointerExit when useFillAmountAnimation:
-                    ResetCoroutine(ref _fillAnimationCoroutine, _fillImage.AnimateFillAmount(0, fillImageDuration, fillImageEaseType));
-                    break;
-            }
+            if(!useFillAmountAnimation) return;
+            if (animationType == AnimationType.PointerEnter)
+                ResetCoroutine(ref _fillAnimationCoroutine, _fillImage.AnimateFillAmount(1, fillImageDuration, fillImageEaseType));
+            else if (animationType == AnimationType.PointerExit)
+                ResetCoroutine(ref _fillAnimationCoroutine, _fillImage.AnimateFillAmount(0, fillImageDuration, fillImageEaseType));
         }
         
         private void AnimationScale(AnimationType animationType)
         {
-            switch (animationType)
-            {
-                case AnimationType.PointerEnter when useScaleAnimationOnHover:
-                    ResetCoroutine(ref _scaleAnimationCoroutine, _rectTransform.AnimateScale(_calculatedScaleOnHover, scaleDurationOnHover, scaleEaseType));
-                    break;
-                case AnimationType.PointerExit when useScaleAnimationOnHover:
-                    ResetCoroutine(ref _scaleAnimationCoroutine, _rectTransform.AnimateScale(_defaultScale, scaleDurationOnHover, scaleEaseType));
-                    break;
-                case AnimationType.PointerDown when useScaleAnimationOnClick:
-                    ResetCoroutine(ref _scaleAnimationCoroutine, _rectTransform.AnimateScale(_calculatedScaleOnClick, scaleDurationOnClick, scaleEaseType));
-                    break;
-                case AnimationType.PointerUp when useScaleAnimationOnClick:
-                    ResetCoroutine(ref _scaleAnimationCoroutine, _rectTransform.AnimateScale(_defaultScale, scaleDurationOnClick, scaleEaseType));
-                    break;
-            }
+            if (animationType == AnimationType.PointerEnter && useScaleAnimationOnHover)
+                ResetCoroutine(ref _scaleAnimationCoroutine, _rectTransform.AnimateScale(_calculatedScaleOnHover, scaleDurationOnHover, scaleEaseType));
+            else if (animationType == AnimationType.PointerExit && useScaleAnimationOnHover)
+                ResetCoroutine(ref _scaleAnimationCoroutine, _rectTransform.AnimateScale(_defaultScale, scaleDurationOnHover, scaleEaseType));
+            else if (animationType == AnimationType.PointerDown && useScaleAnimationOnClick)
+                ResetCoroutine(ref _scaleAnimationCoroutine, _rectTransform.AnimateScale(_calculatedScaleOnClick, scaleDurationOnClick, scaleEaseType));
+            else if (animationType == AnimationType.PointerUp && useScaleAnimationOnClick)
+                ResetCoroutine(ref _scaleAnimationCoroutine, _rectTransform.AnimateScale(_defaultScale, scaleDurationOnClick, scaleEaseType));
         }
         
         private void AnimationColor(AnimationType animationType)
         {
-            switch (animationType)
-            {
-                case AnimationType.PointerEnter when useColorAnimationOnHover:
-                    ResetCoroutine(ref _colorAnimationCoroutine, _image.AnimateColor(colorOnHover, colorDurationOnHover)); 
-                    break;
-                case AnimationType.PointerExit when useColorAnimationOnHover:
-                    ResetCoroutine(ref _colorAnimationCoroutine, _image.AnimateColor(_defaultColor, colorDurationOnHover)); 
-                    break;
-                case AnimationType.PointerDown when useColorAnimationOnClick:
-                    ResetCoroutine(ref _colorAnimationCoroutine, _image.AnimateColor(colorOnClick, colorDurationOnClick)); 
-                    break;
-                case AnimationType.PointerUp when useColorAnimationOnClick:
-                    ResetCoroutine(ref _colorAnimationCoroutine, _image.AnimateColor(_defaultColor, colorDurationOnClick)); 
-                    break;
-            }
+            if (animationType == AnimationType.PointerEnter && useColorAnimationOnHover)
+                ResetCoroutine(ref _colorAnimationCoroutine, _image.AnimateColor(colorOnHover, colorDurationOnHover));
+            else if (animationType == AnimationType.PointerExit && useColorAnimationOnHover)
+                ResetCoroutine(ref _colorAnimationCoroutine, _image.AnimateColor(_defaultColor, colorDurationOnHover));
+            else if (animationType == AnimationType.PointerDown && useColorAnimationOnClick)
+                ResetCoroutine(ref _colorAnimationCoroutine, _image.AnimateColor(colorOnClick, colorDurationOnClick));
+            else if (animationType == AnimationType.PointerUp && useColorAnimationOnClick)
+                ResetCoroutine(ref _colorAnimationCoroutine, _image.AnimateColor(_defaultColor, colorDurationOnClick));
         }
         
         private void ImageChange(AnimationType animationType)
         {
-            switch (animationType)
-            {
-                case AnimationType.PointerEnter when useImageChangerOnHover:
-                    _image.sprite = hoverImage;
-                    break;
-                case AnimationType.PointerExit when useImageChangerOnHover:
-                    _image.sprite = _defaultSprite;
-                    break;
-                case AnimationType.PointerDown when useImageChangerOnClick:
-                    _image.sprite = clickImage;
-                    break;
-                case AnimationType.PointerUp when useImageChangerOnClick:
-                    _image.sprite = _defaultSprite;
-                    break;
-            }
+            if (animationType == AnimationType.PointerEnter && useImageChangerOnHover)
+                _image.sprite = hoverImage;
+            else if (animationType == AnimationType.PointerExit && useImageChangerOnHover)
+                _image.sprite = _defaultSprite;
+            else if (animationType == AnimationType.PointerDown && useImageChangerOnClick)
+                _image.sprite = clickImage;
+            else if (animationType == AnimationType.PointerUp && useImageChangerOnClick) 
+                _image.sprite = _defaultSprite;
         }
         
         private void ResetCoroutine(ref Coroutine coroutine, IEnumerator enumerator)
