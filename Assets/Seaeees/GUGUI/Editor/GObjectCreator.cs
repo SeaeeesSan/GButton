542616199497d10f1c496cd91dcbeb883f8f953d
Seaeees <seaeees@gmail.com>
2021-03-21 09:48:16 +0900

    Fix:_uiResourceが空になる問題を修正
    一部バージョンでエラーが発生するため

--- Assets/Seaeees/GUGUI/Editor/GObjectCreator.cs
+++ Assets/Seaeees/GUGUI/Editor/GObjectCreator.cs
@@ -8,11 +8,23 @@ namespace Seaeees.GUGUI.Editor
     {
         private static DefaultControls.Resources _uiResource;
     
+        private const string SpritePath = "UI/Skin/UISprite.psd";
+        
+        private static DefaultControls.Resources GetResource()
+        {
+            if (_uiResource.standard == null)
+            {
+                _uiResource.standard = AssetDatabase.GetBuiltinExtraResource<Sprite>(SpritePath);
+            }
+            return _uiResource;
+        }
+
         [MenuItem("GameObject/UI/GUGUI/GButton", false, 0)]
         private static void CreateGButton(MenuCommand menuCommand)
         {
+            
             var parent = menuCommand.context as GameObject;
-            GameObject btn = DefaultControls.CreateButton(_uiResource);
+            GameObject btn = DefaultControls.CreateButton(GetResource());
             btn.transform.SetParent(parent.transform);
             btn.AddComponent<GButton>();
             btn.GetComponent<Button>().transition = Selectable.Transition.None;
