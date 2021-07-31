79e2eea634d65c8e743170de9ed8c748ca8dfb21
Seaeees <seaeees@gmail.com>
2021-07-31 10:55:54 +0900

    improve: エディターをタブ形式から変更
    まだインスペクターが見づらいので要調整

---
+++ Assets/Seaeees/GButton/Editor/Styling.cs
@@ -0,0 +1,33 @@
+  
+using UnityEditor;
+using UnityEngine;
+    /// <summary>
+    /// Common styles used for Post-processing editor controls.
+    /// </summary>
+    public static class Styling
+    {
+        /// <summary>
+        /// Style for the override checkbox.
+        /// </summary>
+        public static readonly GUIStyle smallTickbox;
+
+        static readonly Color headerBackgroundDark;
+        static readonly Color headerBackgroundLight;
+
+        /// <summary>
+        /// Color of effect header backgrounds.
+        /// </summary>
+        public static Color headerBackground { get { return EditorGUIUtility.isProSkin ? headerBackgroundDark : headerBackgroundLight; } }
+
+
+        static Styling()
+        {
+            smallTickbox = new GUIStyle("ShurikenToggle");
+
+            headerBackgroundDark = new Color(0.1f, 0.1f, 0.1f, 0.2f);
+            headerBackgroundLight = new Color(1f, 1f, 1f, 0.2f);
+
+        }
+    }
+
+
